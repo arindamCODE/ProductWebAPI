78e122f745c84b4e10023b67cd44b3709865c4a1
Arindam Gohain <sunnyraj24@gmail.com>
2017-10-11 12:38:59 +0530

    Fourth Commit

--- Models/ProductInfo.cs
+++ Models/ProductInfo.cs
@@ -1,13 +1,24 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Models
 {
     public class ProductInfo
     {
         public int ID{get; set;}
+
         public int groupID{get; set;}
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Description { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public int Rate { get; set; }
     }
 }
