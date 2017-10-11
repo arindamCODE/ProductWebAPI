42825e9086b54694ccee125fc2277170441d4331
Arindam Gohain <sunnyraj24@gmail.com>
2017-10-11 12:04:29 +0530

    Third Commit

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 using System.Configuration;
 using System;
+using System.Text.RegularExpressions;
 
 namespace Controllers
 {
@@ -46,40 +47,81 @@ namespace Controllers
         {
             try
             {
-                _context.ProductInfo.Add(item);
+                if(IsAlphaName(item) && IsNumericRate(item))
+                {
+                    _context.ProductInfo.Add(item);
+                }        
                 await _context.SaveChangesAsync();
             }catch(Exception ex){
-                throw new Exception(ex.Message);
+                Console.WriteLine(ex.Message);
             }    
         }
 
         [HttpPut("{id}")]
         public async Task Update(long id, [FromBody] ProductInfo item)
         {
-            var result = _context.ProductInfo.FirstOrDefault(t => t.ID == id);
-            result.Name = item.Name;
-            result.Description = item.Description;
             try
             {
-                _context.ProductInfo.Update(result);
+                var result = _context.ProductInfo.FirstOrDefault(t => t.ID == id);    
+                
+                
+                if(IsAlphaName(item) && IsNumericRate(item))
+                {
+                    result.Name = item.Name;
+                    result.Rate = item.Rate;
+                    result.Description = item.Description; 
+                    _context.ProductInfo.Update(result);
+                }    
                 await _context.SaveChangesAsync();
-            }catch(Exception ex){
-                throw new Exception(ex.Message);
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }    
         }
 
 
         [HttpDelete("{id}")]
         public async Task Delete(long id)
-        {
-            var result = _context.ProductInfo.FirstOrDefault(t => t.ID == id);
+        { 
             try
             {
+                var result = _context.ProductInfo.FirstOrDefault(t => t.ID == id);
                 _context.ProductInfo.Remove(result);
                 await _context.SaveChangesAsync();
             }catch(Exception ex){
                 throw new Exception(ex.Message);
             }    
+        }
+
+        bool IsAlphaName(ProductInfo item)
+        {
+            string pattern = "^[A-Za-z ]+$";
+            try
+            {
+                Regex regex = new Regex(pattern);
+                if(regex.IsMatch(item.Name) == false)
+                    return false;
+                else
+                    return true;
+            }catch(Exception ex){
+                throw new Exception(ex.Message);
+            }            
+        }
+
+        bool IsNumericRate(ProductInfo item)
+        {
+            string pattern = "^[0-9 ]+$";
+            string str = item.Rate.ToString();
+            try
+            {
+                Regex regex = new Regex(pattern);
+                if(regex.IsMatch(str) == false)
+                    return false;
+                else
+                    return true;
+            }catch(Exception ex){
+                throw new Exception(ex.Message);
+            }        
         }
     }
 }
